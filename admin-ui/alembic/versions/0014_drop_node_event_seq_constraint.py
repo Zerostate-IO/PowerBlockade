5227b211296edefe36661bcc0acb0bde5e66db2f
derp <43510052+nulluserid@users.noreply.github.com>
2026-02-02 01:32:41 -0800

    Fix migration revision references

--- admin-ui/alembic/versions/0014_drop_node_event_seq_constraint.py
+++ admin-ui/alembic/versions/0014_drop_node_event_seq_constraint.py
@@ -7,8 +7,8 @@ Create Date: 2026-02-02
 
 from alembic import op
 
-revision = "0014"
-down_revision = "0013"
+revision = "0014_drop_node_event_seq"
+down_revision = "0013_blocklist_entries"
 branch_labels = None
 depends_on = None
 
